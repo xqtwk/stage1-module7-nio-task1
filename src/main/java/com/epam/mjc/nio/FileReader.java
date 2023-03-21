7732bbd11d78db5ec7c0964848c02c6112cb5847
Richard <xqtwkk@gmail.com>
2023-03-21 04:10:03 +0300

    resuming course after 6 months lmao

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,33 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
-
+import java.io.*;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        StringBuilder stringBuilder = new StringBuilder();
+        try (Stream<String> stream = Files.lines(file.toPath())) {
+            stream.forEach((line) -> stringBuilder.append(line).append("\n"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String[] everything = stringBuilder.toString().split("\n");
+        Map<String, String> profileMap = new HashMap<String, String>();
+
+        for (String line : everything) {
+            String[] pair = line.split(": ");
+            profileMap.put(pair[0], pair[1]);
+        }
+
+        return new Profile(
+                profileMap.get("Name"),
+                Integer.parseInt(profileMap.get("Age")),
+                profileMap.get("Email"),
+                Long.parseLong(profileMap.get("Phone"))
+        );
     }
 }
